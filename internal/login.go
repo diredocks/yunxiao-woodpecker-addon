932e47c3104dd36282b807be493d3a64f118306c
diredocks <26994007+diredocks@users.noreply.github.com>
2026-08-08 15:53:24 +0800

    refactor: use go:embed for login page, merge login server into login.go

--- internal/login.go
+++ internal/login.go
@@ -1,9 +1,20 @@
 package internal
 
 import (
+	_ "embed"
 	"log/slog"
 	"net/http"
 )
+
+//go:embed login.html
+var loginPageHTML string
+
+func (f *Forge) LoginHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Cache-Control", "no-store")
+	w.Write([]byte(loginPageHTML))
+}
 
 func (f *Forge) StartLoginServer(port string) {
 	addr := ":" + port
